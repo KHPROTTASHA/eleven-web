88de21e3eefd68adc48a4e4414c0fa1569fbea97
KHPROTTASHA <prottashakhp@gmail.com>
2022-09-17 01:33:31 +0530

    aleix

---
+++ a.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//let fLen = fruits.length;
+console.log(fruits[0]);
+
+for (let i = 0; i < fruits.length; i++) {
+  //console.log(fruits[i])
+}
